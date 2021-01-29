9886aafa95d5bcd046fdd85f75b6970516f60501
QuantumEnigmaa <thibaud.vaisseau@isen.yncrea.fr>
2021-01-29 09:54:52 +0100

    1ère implémentation viewPager avec texte

---
+++ Code/app/src/main/java/fr/isen/vaisseau/androiderestaurant2/FragmentOne.kt
@@ -0,0 +1,45 @@
+package fr.isen.vaisseau.androiderestaurant2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import fr.isen.vaisseau.androiderestaurant2.databinding.ActivityProductBinding
+import fr.isen.vaisseau.androiderestaurant2.databinding.FragmentTemplateBinding
+
+private lateinit var binding: FragmentTemplateBinding
+
+private const val ARG_PARAM1 = "param1"
+
+class FragmentOne : Fragment() {
+    private var param1: String? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentTemplateBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+        }
+
+        binding.fragmentOneText.text = param1
+    }
+
+    companion object {
+        fun newInstance(param1: String): FragmentOne =
+                FragmentOne().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                    }
+                }
+    }
+}
--- Code/app/src/main/java/fr/isen/vaisseau/androiderestaurant2/ProductActivity.kt
+++ Code/app/src/main/java/fr/isen/vaisseau/androiderestaurant2/ProductActivity.kt
@@ -18,8 +18,21 @@ class ProductActivity : AppCompatActivity() {
         binding.activityProductTitle.text = result?.name
         binding.activityProductIngredients.text = result?.getIngredients()
 
-        val imageSrc = result?.getFirstPicture()
+        /*val imageSrc = result?.getFirstPicture()
         Picasso.get().load(imageSrc).placeholder(R.drawable.searching).error(R.drawable.error_image).fit().into(
-            binding.activityProductFoodImage)
+            binding.activityProductFoodImage)*/
+
+        setViewPager()
+    }
+
+    private fun setViewPager() {
+        val adapter = myViewPagerAdapter(supportFragmentManager)
+
+        val firstFragment: FragmentOne = FragmentOne.newInstance("Bonjour test")
+        val seconndFragment: FragmentOne = FragmentOne.newInstance("C'EST PARTI OUI OUI")
+        adapter.addFragment(firstFragment)
+        adapter.addFragment(seconndFragment)
+
+        binding.activityProductViewPager.adapter = adapter
     }
 }
