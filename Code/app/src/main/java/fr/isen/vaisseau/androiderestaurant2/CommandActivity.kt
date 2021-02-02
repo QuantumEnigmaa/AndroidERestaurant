18b92a05382471478c4670ae7ba5d998b1cf41e9
QuantumEnigmaa <thibaud.vaisseau@isen.yncrea.fr>
2021-02-02 18:24:29 +0100

    Finalisation de l'apparence de l'activité de commande avec coroutines

--- Code/app/src/main/java/fr/isen/vaisseau/androiderestaurant2/CommandActivity.kt
+++ Code/app/src/main/java/fr/isen/vaisseau/androiderestaurant2/CommandActivity.kt
@@ -1,23 +1,31 @@
 package fr.isen.vaisseau.androiderestaurant2
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
-import com.google.gson.Gson
+import com.squareup.picasso.Picasso
+import fr.isen.vaisseau.androiderestaurant2.databinding.ActivityCommandBinding
 import fr.isen.vaisseau.androiderestaurant2.model.UserDataJSON
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.json.JSONObject
 import java.io.File
+
+private lateinit var binding: ActivityCommandBinding
 
 class CommandActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_command)
+        binding = ActivityCommandBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        //sendBasket()
+        sendBasket()
     }
 
     private fun sendBasket() {
@@ -27,23 +35,37 @@ class CommandActivity : AppCompatActivity() {
 
         // Get user id
         val sharePreferences = getSharedPreferences(USER_PREF, MODE_PRIVATE)
-        val uid = sharePreferences.getInt(QUANT_PREF, 0)
+        val uid = sharePreferences.getInt(USER_ID, 0)
 
         // Get user Basket
         val file = File(cacheDir.absolutePath+"Basket.json")
         val msg: String = file.readText()
 
         data.put("id_shop", "1")
-        data.put("id_user", uid)
+        data.put("id_user", uid.toString())
         data.put("msg", msg)
 
         val request = JsonObjectRequest(
             Request.Method.POST, url, data,
             {
-                val gson: UserDataJSON = Gson().fromJson(it.toString(), UserDataJSON::class.java)
-                Log.i("api_return_code", gson.return_code.toString())
+                // Cas du succès
+                Log.i("return", it.toString())
+                file.delete()
+                MainScope().launch {
+                    binding.commandImage.visibility = View.GONE
+                    delay(3000)
+                    Log.i("coroutine", "ça marche !")
+                    binding.commandProgressBar.visibility = View.GONE
+                    binding.commandTitle.text = getString(R.string.command_success)
+                    Picasso.get().load(R.drawable.checked).error(R.drawable.error_image).fit().into(
+                        binding.commandImage)
+                    binding.commandImage.visibility = View.VISIBLE
+                }
             },
-            { error -> Log.d("commandError", error.toString()) }
+            {
+                Log.d("commandError", it.toString())
+                binding.commandTitle.text = getString(R.string.command_failure)
+            }
         )
 
         // Add the request to the RequestQueue.
--- Code/app/src/main/java/fr/isen/vaisseau/androiderestaurant2/FoodActivity.kt
+++ Code/app/src/main/java/fr/isen/vaisseau/androiderestaurant2/FoodActivity.kt
@@ -1,18 +1,11 @@
 package fr.isen.vaisseau.androiderestaurant2
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.google.gson.Gson
 import fr.isen.vaisseau.androiderestaurant2.databinding.ActivityFoodBinding
