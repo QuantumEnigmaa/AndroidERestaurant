f47c88b1446c6f93945d854215e2a15c1323925b
QuantumEnigmaa <thibaud.vaisseau@isen.yncrea.fr>
2021-02-03 16:46:21 +0100

    Ajout animation au lancement

---
+++ Code/app/src/main/java/fr/isen/vaisseau/androiderestaurant2/Animations/LoadingAnimation.kt
@@ -0,0 +1,36 @@
+package fr.isen.vaisseau.androiderestaurant2.Animations
+
+import android.app.Activity
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.airbnb.lottie.LottieAnimationView
+import fr.isen.vaisseau.androiderestaurant2.BaseActivity
+
+open class LoadingAnimation constructor(private val context: Activity, animationName: String) {
+    private var lottieAnimationView : LottieAnimationView = LottieAnimationView(context)
+
+    private var cLayout : ConstraintLayout = ConstraintLayout(context)
+    private var cLayoutParams : ConstraintLayout.LayoutParams = ConstraintLayout.LayoutParams(
+            ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT
+    )
+
+    private val lLayoutParams : ConstraintLayout.LayoutParams = ConstraintLayout.LayoutParams(
+            ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT
+    )
+
+    init {
+        lottieAnimationView.setAnimation(animationName)
+        lottieAnimationView.layoutParams = lLayoutParams
+
+        cLayout.addView(lottieAnimationView)
+    }
+
+    fun playAnimation() {
+        context.setContentView(cLayout, cLayoutParams)
+        lottieAnimationView.playAnimation()
+    }
+
+    fun stopAnimation(root: ConstraintLayout) {
+        lottieAnimationView.cancelAnimation()
+        context.setContentView(root)
+    }
+}
--- Code/app/src/main/java/fr/isen/vaisseau/androiderestaurant2/HomeActivity.kt
+++ Code/app/src/main/java/fr/isen/vaisseau/androiderestaurant2/HomeActivity.kt
@@ -4,18 +4,32 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
+import fr.isen.vaisseau.androiderestaurant2.Animations.LoadingAnimation
 import fr.isen.vaisseau.androiderestaurant2.databinding.ActivityHomeBinding
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 private lateinit var binding: ActivityHomeBinding
 const val START_FOOD = "startFood"
 
 class HomeActivity : BaseActivity() {
 
+    private lateinit var loadingAnimation: LoadingAnimation
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
-        setContentView(binding.root);
+        setContentView(binding.root)
+
+        // Loading screen
+        loadingAnimation = LoadingAnimation(this, "loading.json")
+
+        MainScope().launch {
+            loadingAnimation.playAnimation()
+            delay(3000)
+            loadingAnimation.stopAnimation(binding.root)
+        }
 
         // onClick handler
         binding.activityHomeEntree.setOnClickListener {
